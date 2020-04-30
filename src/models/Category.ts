a6ebc6fb184e0e3bbd65c115bfd6d7766c4c7f15
andersonfrfilho <andersonfrfilho@gmail.com>
2020-04-30 08:08:54 -0300

    feature:commit

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -3,12 +3,17 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
+  OneToMany,
 } from 'typeorm';
+import Transaction from './Transaction';
 
 @Entity('categories') // informar que ela é uma instancia do banco de dados da tabela categoria
 class Category {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 
   @Column()
   title: string;
